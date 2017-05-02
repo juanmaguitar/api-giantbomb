6dbeacc4da9119c87a61fc1aceb80817a78b2721
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-05-02 15:45:08 +0200

    fixed

--- app.js
+++ app.js
@@ -18,16 +18,16 @@ app.use(cors())
 app.get('*', function (req, res) {
   const urlRequested = req.originalUrl
   console.log('requesting: ' + urlRequested)
-  if (!urlRequested.includes('favicon.ico')) {
-    options.url = URL_BASE + urlRequested
-    request(options, function (error, response, body) {
-      if (error) res.status(500).send('Something went wrong!')
-      else if (body) res.json(JSON.parse(body))
-      else res.status(404).send('No results!')
-    })
-  } else {
-    res.status(404).send('No results!')
-  }
+  options.url = URL_BASE + urlRequested
+  request(options, function (error, response, body) {
+    if (error) res.status(500).send('Something went wrong!')
+    try {
+      const parsedResult = JSON.parse(body)
+      res.json(parsedResult)
+    } catch (e) {
+      res.status(500).send("Couldn't get a JSON!")
+    }
+  })
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}...`))
